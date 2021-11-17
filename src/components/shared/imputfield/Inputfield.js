4ec036d058e80e8ed264e485cbef250695c71b47
MarinusEIgenraam <rinuseigenraam@gmail.com>
2021-11-17 16:37:18 +0100

    InputField - Dependencies
    - Dependencies - PropTypes
    - Finished the inputfield and added PropTypes to vadidate the given props

--- src/components/shared/imputfield/Inputfield.js
+++ src/components/shared/imputfield/Inputfield.js
@@ -1,28 +1,58 @@
+///////////////////////
+//// Internal
+import React, {useState} from 'react';
 
+///////////////////////
+//// External
+import PropTypes from 'prop-types';
 
-import React from 'react';
-import {validateInput} from '../../../validator/Validator';
-import './Inputfield.css'
+///////////////////////
+//// Internal
+import './Inputfield.css';
+import {validateInput} from "src/validator/Validator";
 
-const Inputfield = ({label,placeholder, className, value, validators, onChange}) => {
+const Inputfield = ({value, label, placeholder, validators, type, onChange, className}) => {
+    const [error, setError] = useState(false);
 
     const handleChange = (event) => {
         const value = event.target;
-        const error = validateInput(validators, value);
+        setError(validateInput(validators, value));
+        onChange(value);
     };
     return (
         <>
             {label && <label htmlFor="app-input-field">{label}</label>}
             <input
                 type="text"
+                value={value}
                 className={className}
                 placeholder={placeholder}
                 onChange={handleChange}/>
+            {error && <span className="text-danger">{error.message}</span>}
         </>
     )
 };
 
 export default Inputfield;
 
+Inputfield.propTypes = {
+    value: PropTypes.string,
+    label: PropTypes.string,
+    placeholder: PropTypes.string,
+    validators: PropTypes.array,
+    type: PropTypes.string,
+    className: PropTypes.string,
+    onChange: PropTypes.func.isRequired,
+};
+
+Inputfield.defaultProps = {
+    value: '',
+    label: '',
+    placeholder: '',
+    validators: '',
+    type: 'text',
+    className: '',
+    onChange: [],
+};
 
 // thank to: https://www.youtube.com/watch?v=HIFxC7Gkgfo&t=284s&ab_channel=CodeWithGhazi
