21a4cb76c40af33ed71ea1adf42488bffadca6e2
MarinusEIgenraam <rinuseigenraam@gmail.com>
2021-11-17 21:08:11 +0100

    Form - hookForm
    - Added a HookForm for later review

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
+///////////////////////
+//// Built-in
 import React, {useState} from "react";
 
-import './App.css';
 ///////////////////////
 //// Internal
+import './App.css';
 import RectangleButton from "./components/shared/buttons/RectangleButton/RectangleButton";
-import InputField from "./components/shared/ImputField/InputField";
-import { Validators } from "./validator/Validator";
 import Form from "./components/layout/Form/Form";
 
 function App() {
   const [value, setValue] = useState(``);
 
-  const handleChange = (value) => {
-    console.log(value);
-    setValue(value);
-  };
 
   return (
       <>
@@ -30,36 +26,7 @@ function App() {
           <h1>
             Imput testfield
           </h1>
-            <InputField
-                value={value}
-                placeholder='TextField'
-                type='text'
-                label='Username'
-                validators={[
-                    {check: Validators.required, message: 'This field is required'}
-                ]}
-                onChange={handleChange}
-            />
-            <InputField
-                value={value}
-                placeholder='NumberField'
-                type='number'
-                label='Number'
-                validators={[
-                    {check: Validators.number, message: 'Number is not valid'}
-                ]}
-                onChange={handleChange}
-            />
-            <InputField
-                value={value}
-                placeholder='EmailField'
-                type='email'
-                label='Email'
-                validators={[
-                    {check: Validators.email, message: 'Email is not valid'}
-                ]}
-                onChange={handleChange}
-            />
+
 
         </header>
       </>
