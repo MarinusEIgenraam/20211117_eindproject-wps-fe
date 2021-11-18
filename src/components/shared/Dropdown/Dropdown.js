a961af48a96735a68e4d2265049f17ee12dc963d
MarinusEIgenraam <rinuseigenraam@gmail.com>
2021-11-18 13:44:59 +0100

    Component - Imputfield
    - created reusabble imputfield component
    
    ! Need to test it and add styles

---
+++ src/components/shared/Dropdown/Dropdown.js
@@ -0,0 +1,57 @@
+////////////////////
+//// Dependencies
+import React from 'react';
+import PropTypes from 'prop-types';
+
+////////////////////
+//// Internal
+import './Dropdown.css'
+
+////////////////////
+//// External
+
+const STYLES = [
+    "drop--primary--solid",
+    "drop--primary--transparant",
+];
+
+const Dropdown = ({value, data, placeholder, listStyle, onChange}) => {
+    const checkListStyle = STYLES.includes(listStyle) ? listStyle : STYLES[0];
+
+    const handleChange = (event) => {
+        console.log(event);
+    };
+
+    return (
+        <>
+            <select value={value} className={checkListStyle} onChange={handleChange}>
+                <option value=''>{placeholder}</option>
+                {data.map((item, key) => (
+                    <option
+                        key={key}
+                        value={item.value}>
+                        {item.label}
+                    </option>
+                ))}
+            </select>
+        </>
+    );
+};
+
+Dropdown.propTypes = {
+    value: PropTypes.string,
+    placeholder: PropTypes.string,
+    data: PropTypes.array.isRequired,
+    className: PropTypes.string,
+    onChange: PropTypes.func.isRequired,
+};
+
+Dropdown.defaultProps = {
+    value: '',
+    placeholder: '',
+    className: '',
+}
+
+export default Dropdown;
+
+/** Created by ownwindows on 18-11-21 **/
